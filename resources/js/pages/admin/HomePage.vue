5486aeb683bea31fdfc31e41cec6a3b42e3c236c
0xArthem <82900721+0xArthem@users.noreply.github.com>
2025-02-23 11:33:10 +0100

    120

--- resources/js/pages/admin/HomePage.vue
+++ resources/js/pages/admin/HomePage.vue
@@ -11,11 +11,69 @@
         </div>
 
         <div v-else>
-            <input
-                type="password"
-                v-model="password"
-                placeholder="Entrez le mot de passe"
-            />
+            <div style="display: flex; justify-content: center">
+                <section
+                    style="width: 400px; align-items: center; display: flex"
+                >
+                    <Input
+                        :type="passwordVisible ? 'text' : 'password'"
+                        v-model="password"
+                        placeholder="Mot de passe"
+                        style="margin-right: 10px"
+                    />
+                    <span
+                        v-if="passwordVisible"
+                        style="cursor: pointer"
+                        @click="togglePasswordVisible"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="lucide lucide-eye"
+                        >
+                            <path
+                                d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
+                            />
+                            <circle cx="12" cy="12" r="3" />
+                        </svg>
+                    </span>
+                    <span
+                        v-else
+                        style="cursor: pointer"
+                        @click="togglePasswordVisible"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="lucide lucide-eye-off"
+                        >
+                            <path
+                                d="M10.733 5.076a10.744 10.744 0 0 1 11.205 6.575 1 1 0 0 1 0 .696 10.747 10.747 0 0 1-1.444 2.49"
+                            />
+                            <path d="M14.084 14.158a3 3 0 0 1-4.242-4.242" />
+                            <path
+                                d="M17.479 17.499a10.75 10.75 0 0 1-15.417-5.151 1 1 0 0 1 0-.696 10.75 10.75 0 0 1 4.446-5.143"
+                            />
+                            <path d="m2 2 20 20" />
+                        </svg>
+                    </span>
+                </section>
+            </div>
+
             <button @click="checkPassword">Valider</button>
             <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
         </div>
@@ -23,12 +81,16 @@
 </template>
 
 <script>
+import { Input } from "../../../../src/components/ui/input";
+
 export default {
+    components: { Input },
     data() {
         return {
             password: "",
             isAuthenticated: localStorage.getItem("isAdmin") === "true",
             errorMessage: "",
+            passwordVisible: false,
         };
     },
     methods: {
@@ -44,6 +106,9 @@ export default {
         logout() {
             localStorage.removeItem("isAdmin");
             this.isAuthenticated = false;
+        },
+        togglePasswordVisible() {
+            this.passwordVisible = !this.passwordVisible;
         },
     },
 };
